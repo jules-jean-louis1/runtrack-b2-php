55581953ba212e385ee062bb3839f8a05fc29e25
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-06 10:10:24 +0200

    job01 done

---
+++ jour-01/job-01/index.php
@@ -0,0 +1,22 @@
+<?php
+/**Dans le dossier job-01, faites un fichier index.php. À l’intérieur de ce fichier index.php,
+ajouter une fonction my_str_search(). Cette fonction permettra de compter le nombre
+d’occurrences d’une lettre dans une chaîne de caractères.
+Elle prendra en premier paramètre la lettre à chercher dans la chaine de caractère, et en
+deuxième paramètre la chaine de caractères dans laquelle chercher. Cette fonction
+retourne un entier, le nombre d’occurrences de la lettre.
+ **/
+
+function my_str_search(string $haystack, string $needle): int {
+    $count = 0;
+    $i = 0;
+    while (isset($haystack[$i])) {
+        if ($haystack[$i] === $needle) {
+            $count++;
+        }
+        $i++;
+    }
+    return $count;
+}
+echo my_str_search("La Plateforme", "a");
+?>
