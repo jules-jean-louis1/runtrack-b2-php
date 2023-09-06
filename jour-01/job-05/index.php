1c5f516e6daa73dca624726e1a6bf0adac518b46
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-06 12:32:14 +0200

    test

--- jour-01/job-05/index.php
+++ jour-01/job-05/index.php
@@ -13,11 +13,6 @@ function my_is_prime(int $number):bool {
 }
 
 echo my_is_prime(3); // true
-echo my_is_prime(6); // false
-
-foreach ([0,1,2,3,4] as &$value) {
-    echo $value . '<br>';
-}
-
+echo my_is_prime(12); // false
 
 ?>
